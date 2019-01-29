609dad093bb5402008dd3c68d0563fb68c150ede
JackRoten <thejackroten@gmail.com>
2019-01-29 01:33:41 -0800

    problem 2 added

--- stupidHWProgram2.py
+++ stupidHWProgram2.py
@@ -3,22 +3,17 @@
 # Prompts user to enter a sentence and print out a count of the number
 # of words and the number of characters in the sentence.
 #
-# Jack Roten 23 Jan 19
+# Jack Roten 29 Jan 19
 #
 #----------------------------------------------------------------------
 
+import string
+from collections import Counter
 
-def count_words_and_letters(sentence):
-    wordList = []
-    letterList = []
-    wordList.append(ii) for ii in wordList
-    wordCount = len(wordList)
-    print(wordCount)
-#    letterList.append(jj) for jj in wordlist
-#    letterCount = len(letterList)
-
-sentence = input('Please enter a sentence')
-pgrmOut = count_words_and_letters(sentence)
+phrase = input('Please enter a sentence: ')
+phrase = phrase.translate(str.maketrans({key: None for key in string.punctuation}))
+print('Number of words in sentence: ', len(phrase.split()))
+print('Number of letters in sentence: ', sum(sorted(map(len, phrase.split()))))
 
 
     
