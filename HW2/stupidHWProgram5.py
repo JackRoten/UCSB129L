a1cbe4918b88dc2a89f58bb5f07e7637f54f5809
JackRoten <thejackroten@gmail.com>
2019-02-06 14:10:34 -0800

    moved HW2 probs into directory

---
+++ HW2/stupidHWProgram5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# StupidHWProgram5.py
+#
+# 129L Homework Exercise 5
+# runs shell /bin/ls /etc command and writes the output to the screen
+# 
+#
+# Jack Roten 29 Jan 19
+#-------------------------------------------------------
+
+import os
+os.system('/bin/ls /etc')
