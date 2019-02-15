779c1fcd73b86148fb191ba234545385c04511b2
JackRoten <thejackroten@gmail.com>
2019-02-15 11:45:47 -0800

    Modifeid dir structure and added completed 1,3,4,5 files to HW4

--- Roten-8831794-hw4/jackrotenexercise1.py
+++ Roten-8831794-hw4/jackrotenexercise1.py
@@ -18,13 +18,13 @@ from ccHistStuff  import statBox, plotErr
 
 dataSet = np.load("dataSet.npy") #Loads numpy data set
 dataArray = np.array(dataSet) #creats a 1D  array populated with dataSet data 
-bins = 16 #
+bins = 17 #
 binEdges = [0,17]
 
 # Plot histogram of data and log scale on y axis. 
 fig = plt.figure() # create a plot object
 ax = fig.add_subplot(1,1,1) # Add a plot space (in this case the whole window)
-hist = plt.hist(dataArray, bins = bins , range = [0,1], density=True) # histogram plot
+hist = plt.hist(dataArray, bins = bins , range = [0,1], normed=True) # histogram plot
 ax.set_yscale('log') #linear to log on y (allows us to visualize distribution better)
 statBox(ax, dataArray, binEdges) # adds statistics box to graph
 plt.show() # have to tell python to show the plot!!
