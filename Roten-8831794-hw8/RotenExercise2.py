0e5d25914d6feed15d2414ffacf2386aaac43501
JackRoten <thejackroten@gmail.com>
2019-03-11 16:19:18 -0700

    added hw8 folder

---
+++ Roten-8831794-hw8/RotenExercise2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Jack Roten
+# PERM#  8831794
+# UCSB Physics 129L
+# Homework 7
+# Python -V = Python 3.7.1
+#
+# Exercise 1
+#
+#--------------------------------------------------------------------------
+
+
